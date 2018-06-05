2530ecb731cac5a0dd7f0d83879eb851a7c4a497
ferminsosa <38511968+ferminsosa@users.noreply.github.com>
2018-06-04 21:19:07 -0300

    Add files via upload

---
+++ estrucflujocontrol/ejercicio301.vb
@@ -0,0 +1,18 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+
+Module ejercicio301
+    Private hoy As Date
+    Sub Main()
+        hoy = Now
+        fecha()
+        hora()
+    End Sub
+    Friend Sub fecha()
+        Console.WriteLine(hoy.Day & "/" & hoy.Month & "/" & hoy.Year)
+    End Sub
+    Friend Function hora() As Byte
+        Console.WriteLine(hoy.Hour & ":" & hoy.Minute & ":" & hoy.Second)
+        Return 0
+    End Function
+End Module
---
+++ estrucflujocontrol/ejercicio303.vb
@@ -0,0 +1,20 @@
+﻿
+Module ejercicio303
+    Private a As Double
+    Sub main()
+        Console.WriteLine("ingrese el valor ")
+        a = Console.ReadLine()
+        For contcinco As Single = 0 To 5 Step 1
+            Dim final As Double
+            final = potencia(a)
+
+        Next
+    End Sub
+    Private Function potencia(ByRef a As Double) As Double
+        a = Math.Pow(a, 2)
+        Console.WriteLine(a)
+        Return a
+    End Function
+
+
+End Module
