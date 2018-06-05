13e2d1366b39bee0aaf09fff411055177b3e28b9
ferminsosa <38511968+ferminsosa@users.noreply.github.com>
2018-06-04 21:23:10 -0300

    Add files via upload

---
+++ estrucflujocontrol/ejercicio304.vb
@@ -0,0 +1,40 @@
+﻿
+Module ejercicio304
+    Sub main()
+        Dim lA As Decimal
+        Dim lB As Decimal
+        Dim res As Int16
+        Console.WriteLine("Ingrese lado A")
+        lA = Console.ReadLine()
+        Console.WriteLine("Ingrese lado B")
+        lB = Console.ReadLine()
+        lA = Math.Floor(lA)
+        lB = Math.Floor(lA)
+        res = validar(lA, lB)
+        Console.WriteLine("La hipotenusa es =" & res)
+    End Sub
+    Private Function validar(ByRef ValA As Int16, ByRef ValB As Int16) As Int16
+        Dim a As Int16 = 1
+        Dim h As Int16
+        While (a = 1)
+            If ValA > 0 And ValB > 0 Then
+                h = Math.Sqrt(Math.Pow(ValA, 2) + Math.Pow(ValB, 2))
+                a = 0
+            Else
+                If ValA <= 0 And ValB <= 0 Then
+                    Console.WriteLine("Ningun valor corresponde a un entero positivo")
+                ElseIf ValB <= 0 Then
+                    Console.WriteLine("El valor B no es entero positivo")
+                Else
+                    Console.WriteLine("El valor A no es entero positivo")
+                End If
+                Console.WriteLine("Ingrese lado A")
+                ValA = Console.ReadLine()
+                Console.WriteLine("Ingrese lado B")
+                ValB = Console.ReadLine()
+                a = 1
+            End If
+        End While
+        Return h
+    End Function
+End Module
---
+++ estrucflujocontrol/ejercicio305.vb
@@ -0,0 +1,37 @@
+﻿
+Module ejercicio305
+    Private Const pi As Decimal = 3.14159265
+    Sub main()
+        Dim base As Single
+        Dim h As Single
+        Console.WriteLine("Ingrese Base R en centimetros ")
+        base = Console.ReadLine()
+        Console.WriteLine("Ingrese Altura H en centimetros ")
+        h = Console.ReadLine()
+        Console.WriteLine("El volumne del cilindro es =" & calculo(base, h) & " en centimetros")
+    End Sub
+    Private Function calculo(ByRef r As Decimal, ByRef h As Decimal) As Decimal
+        Dim a As Byte = 1
+        Dim v As Decimal
+        While (a = 1)
+            If r >= 0 And h >= 0 Then
+                v = Math.Pow(r, 2) * pi * h
+                a = 0
+            Else
+                If r <= 0 And h <= 0 Then
+                    Console.WriteLine("Ningun valor corresponde a positivos")
+                ElseIf h <= 0 Then
+                    Console.WriteLine("La altura H es negativa")
+                Else
+                    Console.WriteLine("La Base R es negativa")
+                End If
+                Console.WriteLine("Ingrese Base R en centimetros ")
+                r = Console.ReadLine()
+                Console.WriteLine("Ingrese Altura H en centimetros ")
+                h = Console.ReadLine()
+                a = 1
+            End If
+        End While
+        Return v
+    End Function
+End Module
